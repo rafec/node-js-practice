f7b7112b468ef842786be6774a1b5481f08379e4
rafec <rafael.brizuena@gmail.com>
2023-03-30 20:51:28 -0400

    added select-user functionality

--- node-js-development-practice/app.js
+++ node-js-development-practice/app.js
@@ -1,16 +1,37 @@
 import http from 'http';
 import queryString from 'query-string';
 import url from 'url';
+import fs from 'fs';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    const params = queryString.parse(url.parse(req.url, true).search);
+    let response;
+    const urlParse = url.parse(req.url, true);
+    const params = queryString.parse(urlParse.search);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
+    if (urlParse.pathname == '/create-user') {
+
+        fs.writeFile(`users/user-${params.id}.txt`, JSON.stringify(params), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(response);
+        });
+        response = 'User created!';
+    } else if (urlParse.pathname == '/select-user') {
+        fs.readFile(`users/user-${params.id}.txt`, function (err, data) {
+            response = data;
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(response);
+        });
+    };
+
 });
 
 server.listen(port, hostname, () => {
