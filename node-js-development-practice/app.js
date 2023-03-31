ac3cb99069a917a60190e3e48ef7b07b59b94b6b
rafec <rafael.brizuena@gmail.com>
2023-03-31 17:30:25 -0400

    added remove-user functionality

--- node-js-development-practice/app.js
+++ node-js-development-practice/app.js
@@ -11,11 +11,10 @@ const server = http.createServer((req, res) => {
     const urlParse = url.parse(req.url, true);
     const params = queryString.parse(urlParse.search);
 
-    if (urlParse.pathname == '/create-user') {
-
+    if (urlParse.pathname == '/create-update-user') {
         fs.writeFile(`users/user-${params.id}.txt`, JSON.stringify(params), function (err) {
             if (err) throw err;
-            console.log('Saved!');
+            response = 'User created/updated!';
 
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/plain');
@@ -30,6 +29,14 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', 'application/json');
             res.end(response);
         });
+    } else if (urlParse.pathname == '/remove-user') {
+        fs.unlink(`users/user-${params.id}.txt`, function (err) {
+            response = err ? 'User not found!' : 'User deleted!';
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(response);
+        });
     };
 
 });
@@ -37,3 +44,8 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// localhost:3000/create-update-user?name=Rafael&age=20&id=1
+// localhost:3000/create-update-user?name=Rafael2&age=25&id=1
+// localhost:3000/select-user?id=1
+// localhost:3000/remove-user?id=1
