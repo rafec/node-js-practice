adedbf7fcc6bb186c8617c5c71c173bd47ac7ae8
rafec <rafael.brizuena@gmail.com>
2023-04-07 19:17:27 -0400

    learning about query strings and http methods

--- express-routes/src/routes/index.js
+++ express-routes/src/routes/index.js
@@ -3,12 +3,18 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
+    console.log(req.query);
+    res.send('Hello World!');
+});
+
+routes.get('/product/:id/:category', (req, res) => {
+    console.log(req.params);
     res.send('Hello World!');
 });
 
 routes.post('/register', (req, res) => {
     console.log(req.body);
-    res.send('Product registered!')
+    res.send('Product registered!');
 });
 
 module.exports = routes;
